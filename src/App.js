56286b872d22f6bc6721bd0d2e0b0c900837197a
unknown <joysen833@gmail,com>
2023-09-04 02:35:03 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './Pages/HomePage';
+import { MyMeetingsPage } from './Pages/MyMeetingsPage';
+import { RoomPage } from './Pages/RoomPage';
+import { LoginPage } from './Pages/LoginPage';
+import { RegisterPage } from './Pages/RegisterPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/register' element={<RegisterPage />} />
+				<Route path='/meetings' element={<MyMeetingsPage />} />
+				<Route path='/room/:roomId' element={<RoomPage />} />
+				<Route path='*' element={<LoginPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
